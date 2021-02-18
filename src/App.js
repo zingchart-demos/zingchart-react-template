2e97311b26c71c54c5b0f3fcea289b0cf21bf9a6
Mike Schultz <mschultz@zingsoft.com>
2021-02-18 08:36:44 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import ZingChart from 'zingchart-react';
+
+const config = {
+    type: 'bar',
+    series: [{
+      values: [4,5,3,4,5,3,5,4,11]
+    }]
+  };
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ZingChart data={config}/>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'zingchart/zingchart-es6.min.js';
 
 ReactDOM.render(
   <React.StrictMode>
